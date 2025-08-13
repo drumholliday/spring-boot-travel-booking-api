ca4117eb6b4e5f2203d169d21628df2638408efb
Drum Holliday <107374333+PurpleBird3@users.noreply.github.com>
2025-08-12 23:48:19 -0500

    added Param import and code on line 15 for FindCountryById using Param(id)

--- src/main/java/com/example/demo/dao/DivisionRepository.java
+++ src/main/java/com/example/demo/dao/DivisionRepository.java
@@ -2,10 +2,15 @@ package com.example.demo.dao;
 
 import com.example.demo.entities.Division;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "divisions", path = "divisions")
 public interface DivisionRepository extends JpaRepository<Division, Long> {
+    // -> /api/divisions/search/findByCountryId?id=1
+    List<Division> findByCountryId(@Param("id") Long countryId);
 }
