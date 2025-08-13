c8baa4b1ce8d71524b40f0721cc401ac08287fd5
Drum Holliday <107374333+PurpleBird3@users.noreply.github.com>
2025-08-12 20:51:55 -0500

    Created CheckoutController for Part H with POST /api/checkout/purchase to place orders. Leave entity CRUD to Spring Data REST; no duplicate controllers (as noted in Part H. CheckoutController is the only controller needed because placing an order is a workflow which comnbines Customer, Cart, and CartItems that isn't a single entity CRUD.

---
+++ src/main/java/com/example/demo/controllers/CheckoutController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controllers;
+
+import com.example.demo.services.CheckoutService; // pulls in the business logic needed.
+import com.example.demo.services.Purchase; // incoming DTO
+import com.example.demo.services.PurchaseResponse; // outgoing DTO
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/checkout") // sets the base URL path for all end points in this controller
+@CrossOrigin("http://localhost:4200") // allows the Angular app running on port 4200 to call this API from the browser (CORS).
+
+public class CheckoutController {
+    // Dependency on the service side layer, final so it must be provided.
+    private final CheckoutService checkoutService;
+    // Constructor injection, Spring 4.3 automatically injects a single controller, no @Autowired needed.
+    // Spring looks for a bean and finds @Service bean from (CheckoutServiceImplementation)and injects it.
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    // Maps HTTP POST requests tp /api/checkout/purchase method
+    // Combine with the mapping base api/checkout and "/purchase" = /api/checkout/purchase.
+    @PostMapping("/purchase")
+
+    // @RequestBody tells Spring to deserialize the JSON body of the request into a Purchase object.
+    // Returns a PurchaseResponse which Spring will serialize back to JSON for the client.
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+        // Hands the Purchase to Service. Returns what the service returns, in this case a PurchaseResponse with tacking number.
+        return checkoutService.placeOrder(purchase);
+    }
+}
